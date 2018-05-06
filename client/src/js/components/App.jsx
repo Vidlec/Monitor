2ca58e06b2b57093447b96a20d0fc26354999d36
Josef Vidlák <vidlec@gmail.com>
2018-05-06 20:49:42 +0200

    Updated react to 16.3.2, Dynamic Icon

--- client/src/js/components/App.jsx
+++ client/src/js/components/App.jsx
@@ -5,23 +5,38 @@ import { Test, Icon } from '@components';
 import { toggleHitStatus } from '@reducers/search/filter/actions';
 
 class App extends Component {
+  state = {
+    iconName: 'eye',
+  };
+
   handleToggleHitStatus = () => {
     const { toggleHitStatus, didHeHitHer } = this.props;
     toggleHitStatus(!didHeHitHer);
   };
 
+  handleToggleIcon = () => {
+    this.setState({ iconName: 'cart' });
+  };
+
   render() {
     const { didHeHitHer } = this.props;
+    const { iconName } = this.state;
 
     return (
       <div className="container">
         <Test />
-        <Icon name="eye" />
+        <Icon name={iconName} />
         <input
           type="button"
           className="btn btn-primary"
           value="Toggle hit"
           onClick={this.handleToggleHitStatus}
+        />
+        <input
+          type="button"
+          className="btn btn-primary"
+          value="Toggle icon"
+          onClick={this.handleToggleIcon}
         />
         {didHeHitHer ? (
           <p>I did hit her :(</p>
--- client/src/js/components/common/Icon.js
+++ client/src/js/components/common/Icon.js
@@ -2,21 +2,26 @@ import React, { Component } from 'react';
 
 class Icon extends Component {
   state = {
-    icon: null,
+    Icon: null,
   };
 
   componentDidMount() {
     this.getIcon(this.props.name);
   }
 
+  componentDidUpdate({ name: prevName }) {
+    const { name } = this.props;
+    if (prevName !== name) this.getIcon(name);
+  }
+
   getIcon = name => {
-    import(`../../../gfx/svg/${name}.svg`).then(pkg =>
-      this.setState({ icon: pkg }),
+    import(`../../../gfx/svg/${name}.svg`).then(Icon =>
+      this.setState({ Icon }),
     );
   };
 
   render() {
-    const { icon: Icon } = this.state;
+    const { Icon } = this.state;
 
     if (Icon) return <Icon.default className={`icon ${this.props.name}`} />;
     return null;
