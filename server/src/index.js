0eded4ce07cbbbef88fdba8d9574b50ee9d2a2bb
Josef Vidlák <vidlec@gmail.com>
2018-05-14 20:44:06 +0200

    Added cassandra-driver

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,26 @@
-const express = require('express');
+import express from 'express';
+import cassandra from 'cassandra-driver';
+
+const client = new cassandra.Client({
+  contactPoints: ['127.0.0.1'],
+});
+
+client
+  .connect()
+  .then(() => {
+    console.log(
+      'Connected to cluster with %d host(s): %j',
+      client.hosts.length,
+      client.hosts.keys(),
+    );
+    console.log('Keyspaces: %j', Object.keys(client.metadata.keyspaces));
+    console.log('Shutting down');
+    return client.shutdown();
+  })
+  .catch(err => {
+    console.error('There was an error when connecting', err);
+    return client.shutdown();
+  });
 
 const app = express();
 
